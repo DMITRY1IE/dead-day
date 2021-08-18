dfd03dd67dae893bb954385d43481a77f27fdbab
DMITRY1IE <75928807+DMITRY1IE@users.noreply.github.com>
2021-08-18 15:05:50 +0300

    fg

--- js/script-2.js
+++ js/script-2.js
@@ -4,15 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log('end');
 });
 
-// let loader = document.getElementById('loader');
-// window.onload = () => {
-//     loader.classList.toggle = ('load-end');
-//     console.log('load-end')
-// }
-// let myGreeting = setTimeout(function () {
-//     alert('Hello, Mr. Universe!');
-// }, 2000)
-
 function counter(ms, className) {
     let counter = 0;
     let interval = setInterval(() => {
@@ -21,3 +12,7 @@ function counter(ms, className) {
     }, ms)
 }
 counter(100, '.res')
+
+let now = new Date();
+const date = document.getElementById('date').innerHTML = (now.getDate() + 1);
+const month = document.getElementById('month').innerHTML = (now.getMonth() + 1);
--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 for (let year = 1920; year <= 2021; year++) {
     let options = document.createElement("OPTION");
+    options.classList.toggle(year);
     document.getElementById("year").appendChild(options).innerHTML = year;
 };
 
+var year_2 = document.getElementById("form").options[n].class;
+
+
+let year_3 = 2021 - year_2;
+console.log(year_3);
+
+if (year_3 >= 18, year_3 <= 35) {
+
+};
+if (year_3 >= 36, year_3 <= 45) {
+
+};
+if (year_3 > 46) {
+
+}
